0448bd10a557e58855ba14b5056fcbd1a63962ef
anajulialeal <lealanju@gmail.com>
2023-11-11 17:53:12 -0300

    first commit

---
+++ scripts/w02-task.js
@@ -0,0 +1,39 @@
+/* W02-Task - Profile Home Page */
+
+/* Step 1 - Setup type tasks - no code required */
+
+/* Step 2 - Variables */
+let fullName = "Ana Julia Leal";
+let currentYear = "2023"
+let profilePicture = 'images/myself.png';
+
+/* Step 3 - Element Variables */
+const nameElement = document.getElementById('name');
+const foodElement = document.getElementById('food');
+const yearElement = document.querySelector('#year');
+const imageElement = document.querySelector('img');
+
+/* Step 4 - Adding Content */
+nameElement.innerHTML = `<strong>${fullName}</strong>`;
+imageElement.setAttribute('src', profilePicture);
+imageElement.setAttribute('alt', `Profile image of ${fullName}`);
+
+/* Step 5 - Array */
+const favFoods = ["Ramen and Cup Noodles", " Spaghetti", " Eggplant "];
+foodElement.innerHTML = favFoods;
+const moreFoods = [" Pizza", " Broccoli"];
+favFoods.push(moreFoods);
+foodElement.innerHTML += `<br>${favFoods}`;
+favFoods.shift();
+foodElement.innerHTML += `<br>${favFoods}`;
+favFoods.pop();
+foodElement.innerHTML += `<br>${favFoods}`;
+
+
+
+
+
+
+
+
+
